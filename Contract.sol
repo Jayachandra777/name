a49fc10199248b30085ed354aac05771067bca1c
Jayachandra777 <124385565+Jayachandra777@users.noreply.github.com>
2023-02-16 22:40:10 +0530

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -1,4 +1,30 @@
 pragma solidity ^0.8.0;
+interface IERC20Token {
+
+  function transfer(address, uint256) external returns (bool);
+
+  function approve(address, uint256) external returns (bool);
+
+  function transferFrom(address, address, uint256) external returns (bool);
+
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address) external view returns (uint256);
+
+  function allowance(address, address) external view returns (uint256);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
+}
+
+contract MovieBookingPlatform {
+
+    uint internal showsLength = 0;
+
+    address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
 
 contract PersistentChalkboard {
 
